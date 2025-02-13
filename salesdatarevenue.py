a2b7c16387c91ced525c7327b3fefb9a8e990e5a
unknown <tayolaiya27@gmail.com>
2025-02-13 21:34:54 +0100

    modification

--- salesdatarevenue.py
+++ salesdatarevenue.py
@@ -6,5 +6,7 @@ sales_data = [
     {"product": "Grapes", "quantity": 3, "price": 4.25},
     {"product": "Mango", "quantity": 6, "price": 2.0},
 ]
-filtered = reduce(lambda x, y : x + y, list(map(lambda sales: sales["quantity"] * sales["price"], list(filter(lambda data : data["product"] != "Grapes", sales_data)))))
+filtered = reduce(lambda x, y : x + y, \
+                  list(map(lambda sales: sales["quantity"] * sales["price"], \
+                                                list(filter(lambda data : data["product"] != "Grapes", sales_data)))))
 print(filtered)
